2b43bef686c76e126cc4fec641cccc5d3b9a5d7f
Swagnarok <darryl.le.630@gmail.com>
2022-04-09 17:20:24 -0700

    Added arrays and started on prompts

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Creating array of values for lowercase, uppercase, numeric, and special characters
+const lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+const number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+const special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "[", "]", ".", ",", ":", ";", "?", "<", ">", "/", "\\", "'", "\"", "~", "-", "+", "=", "_", "`"] 
+
+// Creating function to ask user for parameters
+function choice() {
+  var passlength = window.prompt("Pick a password character length between 8 and 128");
+    if (passlength < 8 || passlength > 128) {
+      window.prompt("That is an incorrect value")
+      choice();
+    }
+    else {
+    }
+  console.log("is this working")
+}
+
 // Defining function for generatePassword
 function generatePassword() {
   console.log("Hit confirm")  // here to confirm things work
@@ -18,6 +36,7 @@ function generatePassword() {
 
 // Write password to the #password input
 function writePassword() {
+  choice();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
