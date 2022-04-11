385d8fd27f13d4bffc184f08763baeb8b28c7b1a
Swagnarok <darryl.le.630@gmail.com>
2022-04-10 17:32:48 -0700

    Finalized code and successfully ran

--- script.js
+++ script.js
@@ -9,56 +9,32 @@ var number = "0123456789"
 var special = "!@#$%^&*(){}[].,:;?<>/\\|'~-+=_`\"" 
 
 // Creating functions for random character selector
-function randomLower() {
-  var lowrando = Math.floor(Math.random() * 26);
-  return lower.slice(lowrando, lowrando + 1);
-}
-
-function randomUpper() {
-  var uprando = Math.floor(Math.random() * 26);
-  return upper.slice(uprando, uprando + 1);
-}
-
-function randomNumber() {
-  var numrando = Math.floor(Math.random() * 10);
-  return number.slice(numrando, numrando + 1);
-}
-
-function randomSpecial() {
-  var specrando = Math.floor(Math.random() * 32);
-  return special.slice(specrando, specrando +1);
-}
-
-console.log(randomLower()); // get rid of after finish coding. Only here to confirm on the console
-console.log(randomUpper()); // get rid of after finish coding. Only here to confirm on the console
-console.log(randomNumber()); // get rid of after finish coding. Only here to confirm on the console
-console.log(randomSpecial()); // get rid of after finish coding. Only here to confirm on the console
-
-// Creating function to ask user for parameters
-// function choice() {
-//   var passlength = window.prompt("Pick a password character length between 8 and 128");
-//       if (passlength < 8 || passlength > 128) {
-//       window.alert("That is an incorrect value, please start over")
-//     }
-//     else {
-//       var lowerchoice = window.confirm("Do you want to include lowercase letters?")
-//       var upperchoice = window.confirm("Do you want to include uppercase letters?")
-//       var numberchoice = window.confirm("Do you want to include numbers?")
-//       var specialchoice = window.confirm("Do you want to include special characters?")
-//         if (lowerchoice, upperchoice, numberchoice, specialchoice === false) {
-//           window.alert("You must select at least one criteria, plese start over")
-//         }
-//         else {
-//         }
-//     }
-
-//   console.log(passlength) // get rid of after finish coding. Only here to confirm on the console
-//   console.log(lowerchoice) // get rid of after finish coding. Only here to confirm on the console
-//   console.log(upperchoice) // get rid of after finish coding. Only here to confirm on the console
-//   console.log(numberchoice) // get rid of after finish coding. Only here to confirm on the console
-//   console.log(specialchoice) // get rid of after finish coding. Only here to confirm on the console
-
+// Ended up not needing these, but keeping around just in case
+// function randomLower() {
+//   var lowrando = Math.floor(Math.random() * 26);
+//   return lower.slice(lowrando, lowrando + 1);
 // }
+
+// function randomUpper() {
+//   var uprando = Math.floor(Math.random() * 26);
+//   return upper.slice(uprando, uprando + 1);
+// }
+
+// function randomNumber() {
+//   var numrando = Math.floor(Math.random() * 10);
+//   return number.slice(numrando, numrando + 1);
+// }
+
+// function randomSpecial() {
+//   var specrando = Math.floor(Math.random() * 32);
+//   return special.slice(specrando, specrando +1);
+// }
+
+// Used during coding to confirm functionality
+// console.log(randomLower()); 
+// console.log(randomUpper()); 
+// console.log(randomNumber()); 
+// console.log(randomSpecial()); 
 
 // Defining function for generatePassword
 function generatePassword() {
@@ -78,12 +54,14 @@ function generatePassword() {
           }
       }
   
-    console.log(passlength) // get rid of after finish coding. Only here to confirm on the console
-    console.log(lowerchoice) // get rid of after finish coding. Only here to confirm on the console
-    console.log(upperchoice) // get rid of after finish coding. Only here to confirm on the console
-    console.log(numberchoice) // get rid of after finish coding. Only here to confirm on the console
-    console.log(specialchoice) // get rid of after finish coding. Only here to confirm on the console
+    // Used during coding to confirm functionality
+    // console.log(passlength) 
+    // console.log(lowerchoice) 
+    // console.log(upperchoice) 
+    // console.log(numberchoice) 
+    // console.log(specialchoice) 
   
+  // Creating new string that combines all possible characters by user criteria. Probably a better way than this, but this suffices
   var combined = ""
     if (lowerchoice === true) {
       var newcombined = combined.concat(lower);
@@ -109,21 +87,23 @@ function generatePassword() {
     else {
       var finalcombined = newestcombined
     }
-  console.log(finalcombined)
 
+  // Used during coding to confirm functionality
+  // console.log(finalcombined)
 
-  
-  console.log("Hit confirm")  // get rid of after finish coding. Only here to confirm on the console
+  // Using for loop to generate a password following all the previous criteria
+    var criterialength = finalcombined.length
+    const generation = []
+    for (let i = 0; i < passlength; i++) {
+      let generandom = Math.floor(Math.random() * criterialength);
+      let passchar = finalcombined.slice(generandom, generandom + 1)
+      generation.push(passchar);
+    }
+    
+    // Used during coding to confirm functionality
+    // console.log(generation);
 
-  // Need to prompt user with criteria for password
-  // We start with asking for password lenght, must be between 8 and 128 characters
-  // Following prompts are to ask of inclusion of lowercase, uppercase, numeric, and special characters
-  // Must ensure that password length is of appropriate lenght (validate user input)
-  // Also ensure the user has confirmed at least one inclusion
-  // Generate a password based on criteria selected
-  // Finally display password on page
-
-  return "We didn't drop the combo yet" // we'll put the password here, but while coding, we'll have sample text to check
+  return generation.join(""); 
 }
 
 // Write password to the #password input
